a89330ea22729060b268301ad89dd498d1f382d8
Yulin <54526797+YulinWang-001@users.noreply.github.com>
2021-02-01 23:50:07 +0000

    Add files via upload

---
+++ Exercise 7/Ex7/computeCentroids.m
@@ -0,0 +1,46 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+for i = 1:K
+    
+    % 矩阵的方法，idx == i的意思是是idx向量的元素为i的位置置1，不为i的置0；
+    % 然后乘以X’就是把对应中心i的X值加起来，最后除以sum及求平均
+    centroids(i,:) = (X'*(idx == i))*(1/(sum(idx == i)));
+    
+end 
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
